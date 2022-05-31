77f87599b1913f560427985f59273047cfd6b602
yjw9393 <74524519+yjw9393@users.noreply.github.com>
2022-05-31 16:11:47 +0900

    Add files via upload

---
+++ projects/딥러닝실제/4주차 과제/Week4_report.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 24 21:40:37 2021
+
+@author: JAEWOONG YOON
+"""
+
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+
+img = imread('2020254011_Jaewoong.png')
+
+plt.imshow(img)
+plt.show()
